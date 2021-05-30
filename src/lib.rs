05ca0830473e274497674c1746215a45ee897bab
hdhoang <code@hdhoang.space>
2021-05-30 17:15:18 +0700

    add clippy, follow its advice

--- src/lib.rs
+++ src/lib.rs
@@ -29,23 +29,24 @@ pub fn exit() -> ! {
     }
 }
 
-/// SAFETY: must be called first in ALL [`cortex_m::entry`] `main` functions
-/// This function prepares the Arm core for running Key firmware:
+/// Prepare the Key MCU for debugging
 /// - Relocate vector table to `0x0800_4000`, after Obins bootloader
 /// cf memory.x from ah-/anne-key and `keyboards\anne_pro\ld\STM32L151.ld` in QMK
 /// - Enable debugging interface, to get defmt RTT working
 /// cf https://github.com/probe-rs/probe-rs/issues/350
+/// # Safety
+/// This function must be called first in ALL [`cortex_m_rt::entry`] `main` functions
 pub unsafe fn setup() {
     use hal::stm32 as core;
 
-    &(*core::SCB::ptr()).vtor.write(0x4000);
+    (*core::SCB::ptr()).vtor.write(0x4000);
 
-    &(*core::DBGMCU::ptr()).cr.modify(|_, w| {
+    (*core::DBGMCU::ptr()).cr.modify(|_, w| {
         w.dbg_sleep().set_bit();
         w.dbg_standby().set_bit();
         w.dbg_stop().set_bit()
     });
-    &(*core::RCC::ptr())
+    (*core::RCC::ptr())
         .ahbenr
         .modify(|_, w| w.dma1en().set_bit());
 }
