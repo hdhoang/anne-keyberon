a392f83c06359c3e440727af712e02b9008e3d65
hdhoang <code@hdhoang.space>
2022-06-25 22:29:36 +0700

    port rtic_messages

--- src/bin/rtic_messages.rs
+++ src/bin/rtic_messages.rs
@@ -4,17 +4,22 @@
 //! Message-passing with different queue depths.
 //! based off <https://github.com/korken89/oxidize2020-rtic/blob/master/examples/7_message_passing.rs>
 
-use anne_keyberon as _; // global logger + panicking-behavior + memory layout
+#[rtic::app(device = hal::stm32, peripherals = true, dispatchers=[EXTI0, EXTI1])]
+mod app {
+    use anne_keyberon as _; // global logger + panicking-behavior + memory layout
 
-#[rtic::app(device = hal::stm32, peripherals = true)]
-const APP: () = {
+    #[shared]
+    struct SharedResources {}
+    #[local]
+    struct LocalResources {}
+
     #[idle]
     fn idle(_cx: idle::Context) -> ! {
         anne_keyberon::idle_loop()
     }
 
-    #[init(spawn=[printer1, printer2])]
-    fn init(cx: init::Context) {
+    #[init]
+    fn init(cx: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
         // - Relocate vector table to `0x0800_4000`, after Obins bootloader
         // cf memory.x from ah-/anne-key and `keyboards\anne_pro\ld\STM32L151.ld` in QMK
         unsafe {
@@ -24,25 +29,21 @@ const APP: () = {
         anne_keyberon::init_debug(cx.device.DBGMCU);
 
         // Print the value via message passing!
-        cx.spawn.printer1(42).ok();
+        printer1::spawn(42).ok();
 
         // This will fail as printer1 has default capacity of 1!
-        if cx.spawn.printer1(43).is_err() {
+        if printer1::spawn(43).is_err() {
             defmt::error!("Second spawn of printer1 failed!");
         }
         // Print to the printer that can take multiple!
-        cx.spawn.printer2(1).ok();
-        cx.spawn.printer2(2).ok();
-        cx.spawn.printer2(3).ok();
-        cx.spawn.printer2(4).ok();
+        printer2::spawn(1).ok();
+        printer2::spawn(2).ok();
+        printer2::spawn(3).ok();
+        printer2::spawn(4).ok();
         defmt::error!("Hello from init! Please wait for the tasks");
+        (SharedResources {}, LocalResources {}, init::Monotonics())
     }
 
-    extern "C" {
-        //! Unmanaged interrupts to spawn priority levels
-        fn EXTI0();
-        fn EXTI1();
-    }
     #[task]
     fn printer1(_cx: printer1::Context, val: u32) {
         defmt::error!("printer1 says {=u32}", val)
@@ -51,4 +52,4 @@ const APP: () = {
     fn printer2(_cx: printer2::Context, val: u32) {
         defmt::error!("printer2 says {=u32}", val)
     }
-};
+}
