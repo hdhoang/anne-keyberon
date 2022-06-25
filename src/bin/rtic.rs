36cc8490fac7f0116277d4f58eceda635579155d
hdhoang <code@hdhoang.space>
2022-06-25 21:31:32 +0700

    continue porting to rtic@1

--- src/bin/rtic.rs
+++ src/bin/rtic.rs
@@ -1,12 +1,17 @@
 #![no_main]
 #![no_std]
 
-use anne_keyberon as _; // global logger + panicking-behavior + memory layout
-
 #[rtic::app(device = hal::stm32, peripherals = true)]
-const APP: () = {
+mod app {
+    use anne_keyberon as _; // global logger + panicking-behavior + memory layout
+
+    #[shared]
+    struct SharedResources {}
+    #[local]
+    struct LocalResources {}
+
     #[init]
-    fn init(cx: init::Context) {
+    fn init(cx: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
         // - Relocate vector table to `0x0800_4000`, after Obins bootloader
         // cf memory.x from ah-/anne-key and `keyboards\anne_pro\ld\STM32L151.ld` in QMK
         unsafe {
@@ -16,5 +21,7 @@ const APP: () = {
         anne_keyberon::init_debug(cx.device.DBGMCU);
 
         defmt::info!("Hello from init! Please interrupt");
+
+        (SharedResources{}, LocalResources{}, init::Monotonics())
     }
-};
+}
