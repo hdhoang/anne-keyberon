5b6f7f2387d5dcd310846f2fc1eb80ddfa941f97
hdhoang <code@hdhoang.space>
2022-06-25 21:51:45 +0700

    rtic_spawn: port to @1

--- src/bin/rtic_spawn.rs
+++ src/bin/rtic_spawn.rs
@@ -4,17 +4,21 @@
 //! Spawn priority-based software tasks.
 //! based off <https://github.com/korken89/oxidize2020-rtic/blob/master/examples/4_spawn_with_priority.rs>
 
-use anne_keyberon as _; // global logger + panicking-behavior + memory layout
+#[rtic::app(device = hal::stm32, peripherals = true, dispatchers= [EXTI0, EXTI1])]
+mod app {
+    use anne_keyberon as _; // global logger + panicking-behavior + memory layout
 
-#[rtic::app(device = hal::stm32, peripherals = true)]
-const APP: () = {
+    #[shared]
+    struct SharedResources {}
+    #[local]
+    struct LocalResources {}
     #[idle]
     fn idle(_cx: idle::Context) -> ! {
         anne_keyberon::idle_loop()
     }
 
-    #[init(spawn=[prio_1_task, prio_2_task])]
-    fn init(cx: init::Context) {
+    #[init]
+    fn init(cx: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
         // - Relocate vector table to `0x0800_4000`, after Obins bootloader
         // cf memory.x from ah-/anne-key and `keyboards\anne_pro\ld\STM32L151.ld` in QMK
         unsafe {
@@ -23,22 +27,18 @@ const APP: () = {
         cx.device.RCC.ahbenr.modify(|_, w| w.dma1en().set_bit());
         anne_keyberon::init_debug(cx.device.DBGMCU);
 
-        cx.spawn.prio_1_task().ok();
-        cx.spawn.prio_2_task().ok();
-        defmt::info!("Hello from init! Please wait for the tasks");
+        prio_1_task::spawn().ok();
+        prio_2_task::spawn().ok();
+        defmt::error!("Hello from init! Please wait for the tasks");
+        (SharedResources {}, LocalResources {}, init::Monotonics())
     }
 
-    extern "C" {
-        //! Unmanaged interrupts to spawn priority levels
-        fn EXTI0();
-        fn EXTI1();
-    }
     #[task]
     fn prio_1_task(_cx: prio_1_task::Context) {
-        defmt::info!("task 1")
+        defmt::error!("task 1")
     }
     #[task(priority = 2)]
     fn prio_2_task(_cx: prio_2_task::Context) {
-        defmt::info!("task 2")
+        defmt::error!("task 2")
     }
-};
+}
